75e270f187b7dfddf9239da9579d6cc2df70491a
Harvey Unsaac <193837@unsaac.edu.pe>
2022-08-16 20:56:34 +0200

    Class TwoWayNode is added into node.py

--- node.py
+++ node.py
@@ -7,3 +7,9 @@ class Node():
     def __init__(self, data: Any, next: Node = None) -> None:
         self.data = data
         self.next = next
+
+
+class TwoWayNode(Node):
+    def __init__(self, data: Any, previous: Node =None, next: Node = None) -> None:
+        super().__init__(data, next)
+        self.previous = previous
